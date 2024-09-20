6848e61c3a44093866f38d2630b50e7777e57b53
chloé-océane harb <146004756+Chloxhrb@users.noreply.github.com>
2024-09-20 11:38:22 +0200

    Update Assistant.py

--- pages/Assistant.py
+++ pages/Assistant.py
@@ -10,46 +10,50 @@ client = OpenAI(
 )
 user_input = st.text_input("Tapez votre texte :")
 prompt = user_input
-chat_completion = client.chat.completions.create(
-    messages=[
-         {
-          "role": "system",
-         "content": f"Tu es un traducteur de langue qui traduit de l'anglais vers le français. Si le texte en entrée n'est pas en anglais, tu retournes la phrase : 'Je ne prends que du texte en anglais.'"
-         },
 
-        {
-            "role": "user",
-            "content": "Hello everyone !",
-        },
-        {
-            "role": "assistant",
-            "content": "La traduction est : Bonjour tout le monde !",
-        },
-         {
-            "role": "user",
-            "content": "Hola todos",
-        },
-        {
-            "role": "assistant",
-            "content": "Je ne prends que du texte en anglais.",
-        },
-         {
-            "role": "user",
-            "content": "Bonjour tout le monde !",
-        },
-        {
-            "role": "assistant",
-            "content": "Je ne prends que du texte en anglais.",
-        },
-         {
-            "role": "user",
-            "content": prompt,
-        },
-    ],
-    model="gpt-3.5-turbo",
-    temperature=0.3,
-  max_tokens=100,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0
-)
+if user_input != "" :
+    chat_completion = client.chat.completions.create(
+        messages=[
+             {
+              "role": "system",
+             "content": f"Tu es un traducteur de langue qui traduit de l'anglais vers le français. Si le texte en entrée n'est pas en anglais, tu retournes la phrase : 'Je ne prends que du texte en anglais.'"
+             },
+    
+            {
+                "role": "user",
+                "content": "Hello everyone !",
+            },
+            {
+                "role": "assistant",
+                "content": "La traduction est : Bonjour tout le monde !",
+            },
+             {
+                "role": "user",
+                "content": "Hola todos",
+            },
+            {
+                "role": "assistant",
+                "content": "Je ne prends que du texte en anglais.",
+            },
+             {
+                "role": "user",
+                "content": "Bonjour tout le monde !",
+            },
+            {
+                "role": "assistant",
+                "content": "Je ne prends que du texte en anglais.",
+            },
+             {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        model="gpt-3.5-turbo",
+        temperature=0.3,
+      max_tokens=100,
+      top_p=1.0,
+      frequency_penalty=0.0,
+      presence_penalty=0.0
+    )
+    
+    st.write(chat_completion.choices[0].message.content)
